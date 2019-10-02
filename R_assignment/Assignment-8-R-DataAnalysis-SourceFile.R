a328a8cc2c7c402e9ab48c9b373a101489ff463b
Myeong Lee <mlee89@N00846.vsnet.gmu.edu>
2019-10-02 17:05:18 -0400

    R data files

---
+++ R_assignment/Assignment-8-R-DataAnalysis-SourceFile.R
@@ -0,0 +1,32 @@
+###--------------------------------------
+#Student Name:
+#GNumber:
+###--------------------------------------
+
+rm(list=ls())
+
+data <- read.csv('EmployeeAttrition.csv')
+
+# a. Find the number of rows and columns in the dataset (5 points)
+
+
+# b. Find the maximum Age in the dataset (5 points)
+
+
+# c. Find the minimum DailyRate in the dataset (5 points)
+
+
+# d. Find the average/mean MontlyIncome in the dataset (5 points)
+
+
+# e. How many employees rated WorkLifeBalance as 1 (5 points)
+
+
+# f. What percent of total employees have TotalWorkingYears less than equal to 5? Also calculate the percentage for TotalWorkingYears greater than 5 (5 points)
+
+
+# g. Print EmployeeNumber, Department and MaritalStatus for those employees whose Attrition is Yes and RelationshipSatisfaction is 1 and YearsSinceLastPromotion is greater than 3 (10 points)
+
+
+# h. Find the mean, median, mode, standard deviation and frequency distribution of EnvironmentSatisfaction for males and females separately. (Hint: For frequency distribution use table() function (10 points)
+ 
