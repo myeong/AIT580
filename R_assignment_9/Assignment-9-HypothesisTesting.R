9411320824abb9cfed7cf3c4f541ff97ee601c45
Myeong Lee <deeper.myeong.lee@gmail.com>
2019-10-09 18:09:43 -0400

    assignment 9

---
+++ R_assignment_9/Assignment-9-HypothesisTesting.R
@@ -0,0 +1,15 @@
+###------------------
+###Hypothesis Testing
+###------------------
+
+###Students Name:
+###GNumber:
+
+setwd("~/git/AIT580/")
+
+rm(list=ls())
+
+data <- read.csv('data/EmployeeAttrition.csv')
+
+
+# Your hypothesis testings here...
