b9023a1d0d9d79ecdcb6066f0c964b5b2958d801
Myeong Lee <deeper.myeong.lee@gmail.com>
2019-11-06 18:40:48 -0500

    week 11

---
+++ R_assignment_13/Assignment-13.R
@@ -0,0 +1,19 @@
+###------------------
+###Regression and Clustering
+###------------------
+
+###Students Name:
+###GNumber:
+
+
+rm(list=ls())
+
+install.packages(ggplot2)
+
+library(ggplot2)
+
+data <- read.csv('EmployeeAttrition.csv')
+
+
+
+# Your answers here...
