db23b8fe309fc54ff3aa4a0d2530963cceb5043e
Myeong Lee <deeper.myeong.lee@gmail.com>
2019-12-03 14:43:13 -0500

    assignment 15 corrected

--- assignment_15/assignment_15.R
+++ assignment_15/assignment_15.R
@@ -10,11 +10,11 @@ titles <- html_nodes(webpage, "div h2")
 without_tags <- gsub("<.*?>", "", titles) 
 print(without_tags)
 
-# Assignment 15 Task 1: write scripts that extract "titles" and "dates" of articles out of the scrapped data. 
+# Assignment 15 Task 1: write scripts that extract "titles" and "news summary" of articles out of the scrapped data. 
 # Then, print them out using "print()" statement. 
 
 
-# Assignment 15 Task 2: write scripts that oraganize your data as dataframe with column names, "title" and "date", respectively.
+# Assignment 15 Task 2: write scripts that oraganize your data as dataframe with column names, "title" and "news summary", respectively.
 # Then, save this dataframe as a CSV file. Name it as "NYT_titles.csv". 
 
 
